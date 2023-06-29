8c6e09fdba568826210e365d2cd1e608cd57155c
FullFontJ <79774839+FullFontJ@users.noreply.github.com>
2023-06-28 21:17:56 -0600

    calculadora en java jdk17

---
+++ src/main/java/com/miempresa/calculadora/Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.miempresa.calculadora;
+
+/**
+ *
+ * @author fullfontj
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        calUI intefazCal = new calUI();
+        
+        intefazCal.show();
+    }
+}
